0e28f4eb698b0885c6e53ed4cd29d9decc1c24df
Dinoxaur <aguspray001@gmail.com>
2022-07-21 15:46:50 +0700

    add: pg config and migrations

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+import {config as dotenv} from 'dotenv';
+
+dotenv();
+
+export default config = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
